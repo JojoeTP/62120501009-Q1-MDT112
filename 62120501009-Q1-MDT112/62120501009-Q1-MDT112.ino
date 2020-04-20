b8917c0441d82ac0443cb76fb9c228ad875baf08
Windwill <jojoeza10@gmail.com>
2020-04-21 01:04:06 +0700

    Update quiz 1 code

--- 62120501009-Q1-MDT112/62120501009-Q1-MDT112.ino
+++ 62120501009-Q1-MDT112/62120501009-Q1-MDT112.ino
@@ -1,6 +1,67 @@
 void setup(){
-
-}
-void loop(){
+    pinMode(2,INPUT_PULLUP);
+    Serial.begin(9600);
+    for(int led = 3 ; led <= 13 ;led++){
+        pinMode(led,OUTPUT);
+    }
+    tone(8,200,500);
+    delay(250);
+    tone(8,400,100);
     
+}
+
+int led = 3,time = 100,count = 1;
+
+void loop(){
+    for(led = 3 ; led <= 13 ; led++){
+        if(led == 8){
+            digitalWrite(led,0);
+            continue;
+        }
+        digitalWrite(led,1);
+        delay(time);
+        digitalWrite(led,0);
+
+        if ( digitalRead(2) == 0 ){
+            // do noting
+            
+            count = count * 2;
+            Serial.println("Faster X 2 (Current Speed : X" + String(count) + ")");
+            tone(8,400,100);
+            time = time / 2;
+            delay(700);
+        }
+        else  {
+            
+            
+        }
+            
+            
+
+    }
+    for(led = 13 ; led >= 3 ; led--){
+        if(led == 8){
+            digitalWrite(led,0);
+            continue;
+        }
+        digitalWrite(led,1);
+        delay(time);
+        digitalWrite(led,0);
+
+        if ( digitalRead(2) == 0 ){
+            // do noting
+            if( digitalRead(2) == 0 ){
+            
+            count = count * 2;
+            Serial.println("Faster X 2 (Current Speed : X" + String(count) + ")");
+            tone(8,400,100);
+            time = time / 2;
+            delay(700);
+            
+            }
+        }
+        else  {
+            
+        }
+    }
 }
